d7ccdb25c0c5e36d59345a9fbe62d5c052d141da
AhmetYldz39 <74781237+AhmetYldz39@users.noreply.github.com>
2025-11-09 13:30:36 +0300

    PTH Reader Added

--- heuristic_bot.py
+++ heuristic_bot.py
@@ -6,14 +6,20 @@ def heuristic_policy(obs):
     aoff = np.pi * obs[2]
     R = 2000.0 * obs[0]
 
-    if brg > np.deg2rad(3): bank_cmd = 2
-    elif brg < -np.deg2rad(3): bank_cmd = 0
-    else: bank_cmd = 1
+    if brg > np.deg2rad(3):
+        bank_cmd = 2
+    elif brg < -np.deg2rad(3):
+        bank_cmd = 0
+    else:
+        bank_cmd = 1
 
     clos = 200.0 * np.arctanh(obs[3])
-    if clos < 0: thr_cmd = 2
-    elif clos > 120: thr_cmd = 0
-    else: thr_cmd = 1
+    if clos < 0:
+        thr_cmd = 2
+    elif clos > 120:
+        thr_cmd = 0
+    else:
+        thr_cmd = 1
 
     inside_wez = (R < 600.0) and (abs(brg) < np.deg2rad(20))
     fire = 1 if (inside_wez and abs(aoff) < np.deg2rad(15)) else 0
