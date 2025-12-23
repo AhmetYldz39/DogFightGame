c40cc709af79e2223d70da8011d2b66309a0ea0d
AhmetYldz39 <74781237+AhmetYldz39@users.noreply.github.com>
2025-12-23 22:37:33 +0300

    Paths are updated

--- test.py
+++
@@ -1,8 +0,0 @@
-from dogfight_env import Dogfight1v1
-
-env = Dogfight1v1()
-print("obs_space:", env.observation_space)
-print("obs_shape:", env.observation_space.shape)
-
-obs = env._obs(0)
-print("obs len:", len(obs), "obs.shape:", obs.shape)
